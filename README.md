654e90aa2a4907753540f2eb8449aec27f60962c
MadByAd <adityaaw84@gmail.com>
2024-05-21 13:30:47 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# MPL Verify
+
+The MPL (MadByAd PHP Library) Verify is library that can be used for verifying form data input. This library contains a list of methods for verifying the incoming form data
---
+++ src/Verify.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * This file is a part of the MadByAd\MPLVerify
+ *
+ * @author    MadByAd <adityaaw84@gmail.com>
+ * @license   MIT License
+ * @copyright Copyright (c) MadByAd 2024
+ *
+ */
+
+namespace MadByAd\MPLVerify;
+
+/**
+ *
+ * The Verify class contains a list of methods for verifying form data
+ *
+ * @author    MadByAd <adityaaw84@gmail.com>
+ * @license   MIT License
+ * @copyright Copyright (c) MadByAd 2024
+ *
+ */
+
+class Verify
+{}
